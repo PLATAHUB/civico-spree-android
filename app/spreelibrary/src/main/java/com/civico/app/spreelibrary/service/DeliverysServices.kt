f4a45cf3075efe6a3f2e081dfba2dec8d63ed684
cahmilo <cahmilo@gmail.com>
2021-02-02 10:28:46 -0500

    get Delivery

--- app/spreelibrary/src/main/java/com/civico/app/spreelibrary/service/DeliverysServices.kt
+++ app/spreelibrary/src/main/java/com/civico/app/spreelibrary/service/DeliverysServices.kt
@@ -2,6 +2,7 @@ package com.civico.app.spreelibrary.service
 
 import com.civico.app.spreelibrary.api.ApiClient
 import com.civico.app.spreelibrary.api.ApiClientDelivery
+import com.civico.app.spreelibrary.api.model.OrderResponse
 import com.civico.app.spreelibrary.model.delivery.DeliveryInformation
 import retrofit2.Call
 import retrofit2.Callback
@@ -11,6 +12,18 @@ object DeliverysServices {
 
     @JvmStatic fun addDelivery(userToken: String, idClient: String, deliveryInformation: DeliveryInformation, dataCallback: DataCallback<DeliveryInformation>)  {
         ApiClientDelivery.apiService.addDelivery(userToken, idClient, deliveryInformation).enqueue(object : Callback<DeliveryInformation> {
+            override fun onResponse(call: Call<DeliveryInformation>, response: Response<DeliveryInformation>) {
+                Utils.executeCorrectResponse(response, dataCallback)
+            }
+
+            override fun onFailure(call: Call<DeliveryInformation>, t: Throwable) {
+                Utils.executeFailedResponse(t, dataCallback)
+            }
+        })
+    }
+
+    @JvmStatic fun getOrders(userToken: String, idClient: String, dataCallback: DataCallback<DeliveryInformation>)  {
+        ApiClientDelivery.apiService.getDelivery(userToken, idClient).enqueue(object : Callback<DeliveryInformation> {
             override fun onResponse(call: Call<DeliveryInformation>, response: Response<DeliveryInformation>) {
                 Utils.executeCorrectResponse(response, dataCallback)
             }
