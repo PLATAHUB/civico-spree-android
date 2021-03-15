6283b3748fc0204dd7ff6817ac65ede20e931215
cahmilo <cahmilo@gmail.com>
2021-03-15 14:30:40 -0500

    Se organizaron endpoints delivery

--- app/spreelibrary/src/main/java/com/civico/app/spreelibrary/service/DeliverysServices.kt
+++ app/spreelibrary/src/main/java/com/civico/app/spreelibrary/service/DeliverysServices.kt
@@ -1,8 +1,6 @@
 package com.civico.app.spreelibrary.service
 
 import com.civico.app.spreelibrary.api.ApiClient
-import com.civico.app.spreelibrary.api.ApiClientDelivery
-import com.civico.app.spreelibrary.api.model.OrderResponse
 import com.civico.app.spreelibrary.model.delivery.DeliveryConfiguration
 import com.civico.app.spreelibrary.model.delivery.DeliveryInformation
 import retrofit2.Call
@@ -11,8 +9,8 @@ import retrofit2.Response
 
 object DeliverysServices {
 
-    @JvmStatic fun addDelivery(city: String, idClient: String, userToken: String, deliveryInformation: DeliveryInformation, dataCallback: DataCallback<DeliveryInformation>)  {
-        ApiClientDelivery.apiService.addDelivery(city, idClient, userToken, deliveryInformation).enqueue(object : Callback<DeliveryInformation> {
+    @JvmStatic fun addDelivery(urlBase:String, city: String, idClient: String, userToken: String, deliveryInformation: DeliveryInformation, dataCallback: DataCallback<DeliveryInformation>)  {
+        ApiClient.getInstance(urlBase).apiService.addDelivery(city, idClient, userToken, deliveryInformation).enqueue(object : Callback<DeliveryInformation> {
             override fun onResponse(call: Call<DeliveryInformation>, response: Response<DeliveryInformation>) {
                 Utils.executeCorrectResponseDelivery(response, dataCallback)
             }
@@ -23,8 +21,8 @@ object DeliverysServices {
         })
     }
 
-    @JvmStatic fun getDelivery(city: String, idClient: String, userToken: String, dataCallback: DataCallback<DeliveryConfiguration>)  {
-        ApiClientDelivery.apiService.getDelivery(city, idClient, userToken).enqueue(object : Callback<DeliveryConfiguration> {
+    @JvmStatic fun getDelivery(urlBase:String, city: String, idClient: String, userToken: String, dataCallback: DataCallback<DeliveryConfiguration>)  {
+        ApiClient.getInstance(urlBase).apiService.getDelivery(city, idClient, userToken).enqueue(object : Callback<DeliveryConfiguration> {
             override fun onResponse(call: Call<DeliveryConfiguration>, response: Response<DeliveryConfiguration>) {
                 Utils.executeCorrectResponseDelivery(response, dataCallback)
             }
